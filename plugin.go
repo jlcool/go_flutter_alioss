06c992ebfe6fda4cfa0d4a72be2dd93ef35776f2
jiangliang <jiangliang.net@gmail.com>
2020-11-19 16:20:39 +0800

    新增上传byte

--- plugin.go
+++ plugin.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/pkg/errors"
+	"bytes"
 )
 
 //  Make sure to use the same channel name as was used on the Flutter client side.
@@ -22,6 +23,7 @@ const (
 	PARAM_KEY	="key"
 	PARAM_ID	="id"
 	PARAM_FILE	="file"
+	PARAM_BYTE	="fileByte"
 	PARAM_PATH	="path"
 )
 type AliossFlutterPlugin struct{
@@ -67,6 +69,7 @@ func (p *AliossFlutterPlugin) InitPlugin(messenger plugin.BinaryMessenger) error
 	p.channel = plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
 	p.channel.HandleFunc("init", handleInit)
 	p.channel.HandleFunc("upload", p.handleUpload)
+	p.channel.HandleFunc("uploadByte", p.handleUploadByte)
 	p.channel.HandleFunc("download", p.handleDownload)
 	p.channel.HandleFunc("secretInit", handleSecretInit)
 	p.channel.HandleFunc("signurl", handleSignurl)
@@ -127,6 +130,66 @@ func (p *AliossFlutterPlugin) handleUpload(arguments interface{}) (reply interfa
 
     // 上传文件流。
     err = bucket.PutObjectFromFile(param_key, param_file,oss.Progress(&OssProgressListener{channel:p.channel,key:param_key,id:param_id}))
+    if err != nil {
+		fmt.Println("Error:", err)
+		m1["message"]=err.Error()
+		m1["result"]="fail"
+		p.channel.InvokeMethod("onUpload", m1)
+        os.Exit(-1)
+	}
+	m1["result"]="success"
+	b, err := json.Marshal(m1)
+	p.channel.InvokeMethod("onUpload",string(b) )
+	return nil, nil
+}
+
+func (p *AliossFlutterPlugin) handleUploadByte(arguments interface{}) (reply interface{}, err error) {
+	var ok bool
+	var args map[interface{}]interface{}
+	if args, ok = arguments.(map[interface{}]interface{}); !ok {
+		return nil, errors.New("invalid arguments")
+	}
+	var param_id string
+	var param_key string
+	var param_bucket string 
+	var param_byte []byte
+
+	if id1, ok := args[PARAM_ID]; ok {
+		param_id = id1.(string)
+	}
+	if key1, ok := args[PARAM_KEY]; ok {
+		param_key = key1.(string)
+	}
+	if bucket1, ok := args[PARAM_BUCKET]; ok {
+		param_bucket = bucket1.(string)
+	}
+	if file1, ok := args[PARAM_BYTE]; ok {
+		param_byte = file1.([]byte)
+	}
+	m1 := make(map[string]string)
+	m1["key"] = param_key
+	m1["id"] = param_id
+	client, err := oss.New(endpoint, accessKeyId, accessKeySecret)
+    if err != nil {
+		fmt.Println("Error:", err)
+		m1["message"]=err.Error()
+		m1["result"]="fail"
+		p.channel.InvokeMethod("onUpload", m1)
+        os.Exit(-1)
+    }
+
+    // 获取存储空间。
+    bucket, err := client.Bucket(param_bucket)
+    if err != nil {
+		fmt.Println("Error:", err)
+		m1["message"]=err.Error()
+		m1["result"]="fail"
+		p.channel.InvokeMethod("onUpload", m1)
+        os.Exit(-1)
+    }
+
+    // 上传文件流。
+    err = bucket.PutObject(param_key,  bytes.NewReader(param_byte),oss.Progress(&OssProgressListener{channel:p.channel,key:param_key,id:param_id}))
     if err != nil {
 		fmt.Println("Error:", err)
 		m1["message"]=err.Error()
